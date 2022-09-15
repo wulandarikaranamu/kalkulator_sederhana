3f9d82daa6286e3dfb1892968e9ffeba6f985fae
wulandarikaranamu02@gmail.com <wulandarikaranamu02@gmail.com>
2022-09-15 07:25:52 -0700

    ubah

--- kalkulator.py
+++ kalkulator.py
@@ -21,27 +21,27 @@ if operasi == "A" or operasi == "a":
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "B" or operasi == "b":
     hasil = bilangan_1 - bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} - {bilangan_2} = {hasil}')
+    print(f'hasil pengurangan adalah : {bilangan_1} - {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "C" or operasi == "c":
     hasil = bilangan_1 * bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} * {bilangan_2} = {hasil}')
+    print(f'hasil perkalian adalah : {bilangan_1} * {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "D" or operasi == "d":
     hasil = bilangan_1 / bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} / {bilangan_2} = {hasil}')
+    print(f'hasil pembagian adalah : {bilangan_1} / {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "E" or operasi == "e":
     hasil = bilangan_1 % bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} % {bilangan_2} = {hasil}')
+    print(f'hasil sisa bagi adalah : {bilangan_1} % {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "F" or operasi == "f":
     hasil = bilangan_1 ** bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} ** {bilangan_2} = {hasil}')
+    print(f'hasil perpangkatan adalah : {bilangan_1} ** {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 elif operasi == "G" or operasi == "g":
     hasil = bilangan_1 // bilangan_2 
-    print(f'hasil penjumlahan adalah : {bilangan_1} // {bilangan_2} = {hasil}')
+    print(f'hasil pembagian bulat adalah : {bilangan_1} // {bilangan_2} = {hasil}')
     print("Kalkulator Berhasil, Terima Kasih !")
 else :
     print("operasi yang dimasukkan tidak sesuai!") 
